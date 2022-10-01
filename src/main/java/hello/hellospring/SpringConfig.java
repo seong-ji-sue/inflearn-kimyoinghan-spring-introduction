b1394931e76449502679278c573661e1b20f7bdf
jisue <tjdwltn121@gmail.com>
2022-10-01 22:24:20 +0900

    10/01 jpa database 방식

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -7,21 +7,24 @@ import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
     private final DataSource dataSource;
     private final EntityManager em;
-    public SpringConfig(DataSource dataSource, EntityManager em) {
+    public SpringConfig(MemberRepository memberRepository, DataSource dataSource, EntityManager em) {
+        this.memberRepository = memberRepository;
         this.dataSource = dataSource;
         this.em = em;
     }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
-// return new MemoryMemberRepository();
-// return new JdbcMemberRepository(dataSource);
-// return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//// return new MemoryMemberRepository();
+//// return new JdbcMemberRepository(dataSource);
+//// return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//
+//    }
 }
